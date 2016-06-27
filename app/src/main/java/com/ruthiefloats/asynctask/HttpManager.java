9ccad26f36d68d7ebbff6afa333c6b60299b3c67
princessruthie <missruthie@gmail.com>
2016-06-27 18:17:53 -0400

    Update HttpManager class to use HttpUrlConnection.

--- app/src/main/java/com/ruthiefloats/asynctask/HttpManager.java
+++ app/src/main/java/com/ruthiefloats/asynctask/HttpManager.java
@@ -1,11 +1,17 @@
 package com.ruthiefloats.asynctask;
 
 import android.net.http.AndroidHttpClient;
-import android.net.http.HttpResponseCache;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class HttpManager {
 
@@ -14,31 +20,36 @@ public class HttpManager {
      * @return
      */
     public static String getData(String uri) {
-
-        /*
-        you had to back down to level 22 to follow along with the
-        deprecated methods.  as of 23 you just plain can't use them
-        the lesson has utility because now i'll understand legacy
-        code and can more sensibly update deprecated code.
-         */
-        AndroidHttpClient client = AndroidHttpClient.newInstance("AndroidAgent");
-        HttpGet request = new HttpGet(uri);
-        HttpResponse response;
+        BufferedReader reader = null;
 
         try {
-            /*
-            response is whatever the client gets back after
-            executing the request
-             */
-            response = client.execute(request);
-            //turn response into a String
-            return EntityUtils.toString(response.getEntity());
-        } catch (Exception e) {
+            URL url = new URL(uri);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            //todo Whats pros cons of specifying MalformedURLException over just Exception?
+            //ah, wait, when you added the httpurlconnection, compiler suggested IOException.
+            StringBuilder sb = new StringBuilder();
+            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            return  sb.toString();
+        } catch (java.io.IOException e) {
             e.printStackTrace();
             return null;
         } finally {
-            //prevent leaks
-            client.close();
+            if (reader != null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
         }
+
     }
+
 }
